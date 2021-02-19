998dde8be696da07efb0b2053474406655bdb3cf
Vasyl Nikitiuk <vnikityuk@competo.io>
2021-02-19 11:32:04 +0200

    Initial Commit

---
+++ FileProcess/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FileProcess
+//
+//  Created by Vasyl Nikitiuk on 19.02.2021.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FileProcess/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  FileProcess
+//
+//  Created by Vasyl Nikitiuk on 19.02.2021.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
